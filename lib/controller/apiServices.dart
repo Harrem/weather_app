2dff6258e1dddd03e11b12db5f540deebdd82268
haw2ang <85292588+haw2ang@users.noreply.github.com>
2022-07-25 16:03:43 +0300

    searchscreen

--- lib/controller/apiServices.dart
+++ lib/controller/apiServices.dart
@@ -4,18 +4,17 @@ import 'package:http/http.dart';
 import 'package:weather_app/model/currentWeather.dart';
 
 class ApiServices {
-  String cityName = "Sayyid Sadiq";
-  String apiKey = "902833916c454c328aa105039221706";
-
-  Future<Post> getJsons() async {
+  Future<Post> getJsons(String cityName) async {
+    String apiKey = "902833916c454c328aa105039221706";
     final url =
         "http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$cityName&aqi=no";
-    Response response = await get(Uri.parse(url));
+
+    final response = await get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       return Post.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('failed');
+      throw Exception();
     }
   }
 }
