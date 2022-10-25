23316ca2dd12dae36ca474c8c4d79468127c6fe4
Harrem <52822616+Harrem@users.noreply.github.com>
2022-10-25 16:39:48 +0300

    changes primary color

--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -5,6 +5,19 @@ class CustomTheme {
   CustomTheme();
   static ThemeData get lightTheme {
     return ThemeData(
+      colorScheme: const ColorScheme(
+        background: Colors.white,
+        brightness: Brightness.light,
+        primary: Color.fromARGB(255, 107, 38, 240),
+        onPrimary: Colors.white,
+        secondary: Colors.indigo,
+        onSecondary: Colors.white,
+        error: Colors.red,
+        onError: Colors.pink,
+        onBackground: Colors.green,
+        surface: Colors.black,
+        onSurface: Colors.grey,
+      ),
       fontFamily: GoogleFonts.poppins().fontFamily,
       inputDecorationTheme: InputDecorationTheme(
         fillColor: Colors.white,
